074e0c40e8983605a90d8d415cab14c9441a9a74
enthusiasitSirjan <sirjanbaniya@gmail.com>
2021-04-21 21:34:14 +0545

    Changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import * as serviceWorker from "../public/service-worker";
+import * as serviceWorker from "../public/serviceworker";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,4 +14,3 @@ serviceWorker.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
