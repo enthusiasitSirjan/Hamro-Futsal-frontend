a40be5be4baa1024115ab1ace88539cb91eee188
enthusiasitSirjan <sirjanbaniya@gmail.com>
2021-04-21 21:23:05 +0545

    Changes

---
+++ public/service-worker.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-restricted-globals */
+// Installing service worker
+const CACHE_NAME = "Hamro Futsal";
+
+/* Add relative URL of all the static content you want to store in
+ * cache storage (this will help us use our app offline)*/
+let resourcesToCache = ["./", "./img/game.png", "./game.js", "./styles.css"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(resourcesToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// Update a service worker
+const cacheWhitelist = ["Hamro-futsal"];
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
